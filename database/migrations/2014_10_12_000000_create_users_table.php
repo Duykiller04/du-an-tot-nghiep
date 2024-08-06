7ea1d353a9e17f06a3a2b848b268c408b2f27f08
Duykiller04 <haijano04@gmail.com>
2024-08-07 00:04:09 +0700

    Model và migration bảng user, customer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone', 20)->nullable();
+            $table->string('address')->nullable();
+            $table->date('birth')->nullable()->comment('Năm sinh');
+            $table->string('image')->nullable();
+            $table->string('description')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('type')->comment('Nhân viên và Người quản lý');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
