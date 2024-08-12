a80e9a319198b969e5d5ba3807164135c9c24c9e
Duykiller04 <117673000+Duykiller04@users.noreply.github.com>
2024-08-13 01:12:22 +0700

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(15),
+            'name' => fake()->name(),
             'parent_id' => null,
             'is_active' => true,
         ];
