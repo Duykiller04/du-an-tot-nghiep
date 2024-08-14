c634d9f81e649f909796801fdb9b00c671be049c
DucNM2004 <minhduc8883@gmail.com>
2024-08-14 11:51:45 +0700

    done quan ly benh

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeseaseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.layouts.master');
 });
+
+Route::prefix('admin')
+    ->as('admin.')
+    ->group(function () {
+
+        Route::controller(DeseaseController::class)
+        ->prefix('diseases')->as('diseases.')
+        ->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/add', 'store')->name('store');
+            Route::get('/{id}', 'edit')->name('edit');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+        });
+
+    });
