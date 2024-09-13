2d35f5ce5141d760489aa5b306ef96649bdc94ec
Duykiller04 <haijano04@gmail.com>
2024-09-14 00:02:44 +0700

    Logo + DataTable Server Side bảng Bệnh + fix responsive

--- resources/views/admin/layouts/partials/side-bar.blade.php
+++ resources/views/admin/layouts/partials/side-bar.blade.php
@@ -4,19 +4,19 @@
         <!-- Dark Logo-->
         <a href="index.html" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="/theme/admin/assets/images/logo-sm.png" alt="" height="22">
+                <img src="/theme/admin/assets/images/logo-sm.png" alt="" height="42">
             </span>
             <span class="logo-lg">
-                <img src="/theme/admin/assets/images/logo-dark.png" alt="" height="17">
+                <img src="/theme/admin/assets/images/logo-dark.png" alt="" height="37">
             </span>
         </a>
         <!-- Light Logo-->
         <a href="index.html" class="logo logo-light">
             <span class="logo-sm">
-                <img src="/theme/admin/assets/images/logo-sm.png" alt="" height="22">
+                <img src="/theme/admin/assets/images/logo-sm.png" alt="" height="42">
             </span>
             <span class="logo-lg">
-                <img src="/theme/admin/assets/images/logo-light.png" alt="" height="17">
+                <img src="/theme/admin/assets/images/logo-light.png" alt="" height="37">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
@@ -253,7 +253,7 @@
                         </ul>
                     </div>
                 </li>
-                
+
 
 
                 {{-- ---------------------------------------------------------------------------------------------------------------- --}}
