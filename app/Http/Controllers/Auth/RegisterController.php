159333b88f9bd6936c4a29ccbef61685f4303ee8
Duykiller04 <haijano04@gmail.com>
2024-10-16 10:18:09 +0700

    auth login, logout

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/diseases';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
