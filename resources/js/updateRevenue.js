c35455645a241b569f2eb9cf39872279e8b68172
DucNM2004 <minhduc8883@gmail.com>
2024-12-03 09:32:54 +0700

    tạo bản ghi điểm danh khi tạo ca làm

--- resources/js/updateRevenue.js
+++ resources/js/updateRevenue.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
 
-
 window.Echo.channel('shift.' + shiftId)
     .listen('RevenueUpdated', function(data) {
         console.log('Doanh thu đã được cập nhật:', data.revenueSummary);
