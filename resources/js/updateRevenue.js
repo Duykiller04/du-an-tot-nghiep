27f53834a7e939e5e591ca68caa0ef02b131ac9f
DucNM2004 <minhduc8883@gmail.com>
2024-12-04 09:31:37 +0700

    sửa lại format tiền reltime và check ko 2 ca cùng mở

--- resources/js/updateRevenue.js
+++ resources/js/updateRevenue.js
@@ -1,8 +1,20 @@
 import './bootstrap';
 
 
+// Hàm định dạng số với "VND" ở cuối
+function formatCurrency(amount) {
+    return new Intl.NumberFormat('vi-VN', {
+        style: 'currency',
+        currency: 'VND',
+        minimumFractionDigits: 0,
+    }).format(amount).replace('VND'); 
+}
+
+// Lắng nghe sự kiện
 window.Echo.channel('shift.' + shiftId)
-    .listen('RevenueUpdated', function(data) {
+    .listen('RevenueUpdated', function (data) {
         console.log('Doanh thu đã được cập nhật:', data.revenueSummary);
-        document.getElementById('total-revenue').textContent = data.revenueSummary;
+
+        const revenueElement = document.getElementById('total-revenue');
+        revenueElement.textContent = formatCurrency(data.revenueSummary);
     });
